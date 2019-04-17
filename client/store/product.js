816d668d89d461b55917425faf00ec2f21fa8c70
Chelsi Asulin <chelsiasulin@gmail.com>
2019-04-17 15:56:39 -0400

    feature: single cake component and reducer added.

--- client/store/product.js
+++ client/store/product.js
@@ -1,19 +1,32 @@
 import axios from 'axios'
-import {runInNewContext} from 'vm'
 
 //Action Type
 export const GOT_CAKES = 'GOT_CAKES'
+export const GOT_SINGLE_CAKE = 'GOT_SINGLE_CAKE'
 
 //Action Creator
 export const gotCakes = cakes => {
   return {type: GOT_CAKES, cakes}
 }
 
+export const gotSingleCake = cake => {
+  return {type: GOT_SINGLE_CAKE, cake}
+}
+
 //Thunks
 export const getCakes = () => async dispatch => {
   try {
-    const {data} = await axios.get('api/cakes') //change depending on route
+    const {data} = await axios.get('/api/cakes')
     dispatch(gotCakes(data))
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+export const getSingleCake = id => async dispatch => {
+  try {
+    const {data} = await axios.get(`/api/cakes/${id}`)
+    dispatch(gotSingleCake(data))
   } catch (err) {
     console.error(err)
   }
@@ -21,16 +34,17 @@ export const getCakes = () => async dispatch => {
 
 //State
 const initialState = {
-  cakes: []
+  cakes: [],
+  singleCake: {}
 }
 
 //Reducer
-
 export default (state = initialState, action) => {
   switch (action.type) {
     case GOT_CAKES:
-      console.log('in the reducer', action.cakes)
-      return action.cakes
+      return {...state, cakes: action.cakes}
+    case GOT_SINGLE_CAKE:
+      return {...state, singleCake: action.cake}
     default:
       return state
   }
